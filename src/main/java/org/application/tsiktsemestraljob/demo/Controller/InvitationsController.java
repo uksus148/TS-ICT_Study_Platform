970fbb209a3111de05f56300013ab08e7475fdb2
uksus148 <kunteynir161@gmail.com>
2025-12-10 03:14:28 +0100

    JUnit tests, fixes.

--- src/main/java/org/application/tsiktsemestraljob/demo/Controller/InvitationsController.java
+++ src/main/java/org/application/tsiktsemestraljob/demo/Controller/InvitationsController.java
@@ -1,5 +1,6 @@
 package org.application.tsiktsemestraljob.demo.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.application.tsiktsemestraljob.demo.DTO.InvitationsDTO.InvitationsMapper;
 import org.application.tsiktsemestraljob.demo.DTO.InvitationsDTO.InviteAcceptDTO;
@@ -16,18 +17,30 @@ public class InvitationsController {
 
     private final InvitationsService invitationsService;
 
+    @Operation(
+            summary = "Creation of invite",
+            description = "This endpoint implement logic that help to create an invitation to group (in token form)"
+    )
     @PostMapping("/{groupId}")
     public InviteCreateResponseDTO createInvitation(@PathVariable Long groupId) {
         Invitations invitation = invitationsService.create(groupId);
         return InvitationsMapper.toCreateDTO(invitation);
     }
 
+    @Operation(
+            summary = "Validate token",
+            description = "This endpoint implement logic that help to validate token and check"
+    )
     @GetMapping("/validate/{token}")
     public InviteValidateDTO validateInvitation(@PathVariable String token) {
         Invitations invitation = invitationsService.validateToken(token);
         return InvitationsMapper.toValidateDTO(invitation);
     }
 
+    @Operation(
+            summary = "Accept token",
+            description = "This endpoint implement logic that help to accept invitation to join in group"
+    )
     @PostMapping("/accept/{token}")
     public InviteAcceptDTO acceptInvitation(@PathVariable String token) {
         Invitations invitation = invitationsService.acceptInvitation(token);
